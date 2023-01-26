60192b6bb6018c4808d96069aee2ca2f4f2c2c5d
JuliansRecent <juliansrecent@gmail.com>
2023-01-25 22:00:43 -0800

    Module complete. Create a prompt for the user to submit a number to, if the number is equal to 1984, output 'Orwell'.

--- src/main/java/Orwell.java
+++ src/main/java/Orwell.java
@@ -4,8 +4,15 @@ import java.util.Scanner;
 public class Orwell {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        // Prompt the user for an integer
+        System.out.println("Give a number:");
+        int number = Integer.valueOf(scanner.nextLine());
+        // Print the string Orwell if the number is exactly 1984
+        if (number == 1984) {
+            System.out.println("Orwell");
+        }
     }
 }
